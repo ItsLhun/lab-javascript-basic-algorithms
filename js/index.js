6159a802bcbc0b3a7df285119140fa8f0df55499
itslhun <nahuelghastin@outlook.com>
2021-06-05 16:22:16 +0200

    complete bonus 1

--- js/index.js
+++ js/index.js
@@ -47,3 +47,23 @@ switch(true) {
     default:
       console.log("What?! You both have the same name?");
 }
+
+
+// bonus 1
+
+// did I just cheat removing the paragraph break? 
+let paragraphs = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet pellentesque libero, non vestibulum tortor luctus vitae. Praesent magna purus, aliquet ut sollicitudin eu, tincidunt aliquet ligula. Etiam eu quam mi. Vivamus fermentum mi sit amet vehicula dictum. Cras auctor lobortis magna, ut egestas massa hendrerit quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lobortis consequat pharetra. Pellentesque rutrum justo orci, eu vehicula ligula suscipit sed. Phasellus placerat erat leo, at posuere nisl placerat ut. Suspendisse posuere commodo suscipit. Proin scelerisque, neque a suscipit eleifend, tellus ipsum varius ipsum, id dapibus purus neque porta felis. Vestibulum arcu ex, dapibus in rhoncus id, tincidunt et eros. Duis consequat turpis a dolor sollicitudin, a fermentum sem suscipit. Pellentesque ullamcorper orci sed consectetur malesuada. Maecenas vulputate rhoncus auctor. Duis sit amet est dapibus, placerat sem dictum, porttitor elit. Etiam non tortor commodo, venenatis sem euismod, tincidunt ligula. Vestibulum pretium lorem magna, ac blandit nunc sodales nec. Ut tellus massa, vehicula placerat vulputate vel, efficitur a arcu. Vestibulum volutpat neque mauris, a efficitur massa tristique a. Duis tristique eget risus et commodo. Mauris vestibulum, elit a varius ultricies, mauris enim luctus mauris, vel ullamcorper enim lacus vitae lectus. Donec commodo justo ut odio interdum, eget molestie nunc eleifend. Aliquam ante purus, tempus maximus placerat euismod, faucibus ut lectus. Nam commodo eleifend ante, sit amet vehicula risus tincidunt non. Nullam consectetur facilisis ligula, eu finibus mi tempus et. Maecenas suscipit, est sit amet euismod sodales, elit urna suscipit turpis, ac interdum eros nulla vel tortor. In purus neque, auctor tincidunt sagittis pulvinar, maximus imperdiet metus. Nulla tellus massa, volutpat et leo non, tincidunt scelerisque lectus. Nam rutrum sapien a turpis sodales consectetur. Vivamus aliquam massa id lectus blandit fringilla. Donec facilisis pulvinar lectus non consectetur.";
+
+let wordsArray = paragraphs.toLowerCase().split(" ");
+
+console.log(wordsArray.length);
+
+
+let etCount = 0;
+for (let word of wordsArray){
+  if (word === "et" || word === "et."){ // this feels wonky
+    etCount++;
+  }
+}
+
+console.log(etCount);
